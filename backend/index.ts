3e2d5b005101ff3527bdabb206450ee6aafaa8d9
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-21 12:56:09 -0400

    Added simple routes for posting and receiving config data, in Express app

--- backend/index.ts
+++ backend/index.ts
@@ -17,3 +17,39 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`);
 });
+
+// Routes
+
+// Data (config array)
+type Config = {
+	xVelocity: number;
+	yVelocity: number;
+};
+
+let data: Config[] = [];
+
+// Post to config array
+app.post("/api/config", (req, res) => {
+	// Consoles
+	console.log("POST: api/config");
+	console.log(req.body);
+
+	// Logic
+	const { xVelocity, yVelocity } = req.body;
+	const newConfig = {
+		xVelocity: Number(xVelocity),
+		yVelocity: Number(yVelocity),
+	};
+	data.push(newConfig);
+	res.status(200).json({ data: data });
+});
+
+// Get config array
+app.get("/api/config", (req, res) => {
+	// Consoles
+	console.log("GET: api/config");
+	console.log(data);
+
+	// Logic
+	res.status(200).json({ data: data });
+});
