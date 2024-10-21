a186f89304816bb0b903fec37c7f557e8a82fe99
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-21 12:40:12 -0400

    Initialized express server

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
