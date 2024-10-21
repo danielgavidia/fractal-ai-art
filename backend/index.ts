c6ae947aefe7331fda67e06c526e8e0756cdee68
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-21 12:44:54 -0400

    Added Dockerfile, .dockerignore, and docker-compose.yaml for backend

--- backend/index.ts
+++ backend/index.ts
@@ -3,7 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 
-app.use(cors());
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL,
+		methods: ["GET", "POST"],
+	})
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
