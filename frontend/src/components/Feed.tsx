20c939b3baf01d8f70c7f02b568dbb2171e05c1f
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-21 12:27:02 -0400

    Created config array default

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import BouncingBall from "./BouncingBall";
 
+const configArrayDefault = [
+	{ xVelocicy: 2, yVelocity: 2 },
+	{ xVelocicy: 4, yVelocity: 4 },
+	{ xVelocicy: 6, yVelocity: 6 },
+	{ xVelocicy: 8, yVelocity: 8 },
+];
+
 const Feed = () => {
-	const configArray = [
-		{ xVelocicy: 2, yVelocity: 2 },
-		{ xVelocicy: 4, yVelocity: 4 },
-		{ xVelocicy: 6, yVelocity: 6 },
-		{ xVelocicy: 8, yVelocity: 8 },
-	];
+	const [configArray, setConfigArray] = useState(configArrayDefault);
 	return (
 		<div>
 			{configArray.map((config, key) => {
