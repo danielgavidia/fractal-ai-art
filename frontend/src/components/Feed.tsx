791b22ecf8a008a6468c44ca7095f93c051459bc
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-21 13:14:24 -0400

    Added util function to Feed component

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,29 +1,26 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 import BouncingBall from "./BouncingBall";
-
-// const configArrayDefault = [
-// 	{ xVelocicy: 2, yVelocity: 2 },
-// 	{ xVelocicy: 4, yVelocity: 4 },
-// 	{ xVelocicy: 6, yVelocity: 6 },
-// 	{ xVelocicy: 8, yVelocity: 8 },
-// ];
+import { getConfigs } from "../utils/getConfigs";
+import { Config } from "../types/types";
 
 const Feed = () => {
-	// const [configArray, setConfigArray] = useState(configArrayDefault);
+	const [configArray, setConfigArray] = useState<Config[]>([]);
 
-	const configArray = [
-		{ xVelocicy: 2, yVelocity: 2 },
-		{ xVelocicy: 4, yVelocity: 4 },
-		{ xVelocicy: 6, yVelocity: 6 },
-		{ xVelocicy: 8, yVelocity: 8 },
-	];
+	// Reload configs
+	useEffect(() => {
+		const fetch = async () => {
+			const res = await getConfigs();
+			setConfigArray(res);
+		};
+		fetch();
+	}, []);
 
 	return (
 		<div>
 			{configArray.map((config, key) => {
 				return (
 					<div className="w-full flex justify-center pb-2">
-						<BouncingBall key={key} xVelocity={config.xVelocicy} yVelocity={config.yVelocity} />
+						<BouncingBall key={key} xVelocity={config.xVelocity} yVelocity={config.yVelocity} />
 					</div>
 				);
 			})}
