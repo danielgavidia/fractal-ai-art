f0d8face9aa44b90bb22ad491aac693df1afefa6
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-23 21:43:48 -0400

    Changed styling for feed component

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -3,12 +3,12 @@ import Navbar from "../components/Navbar";
 
 const Root = () => {
   return (
-    <div className="w-full bg-background text-foreground">
+    <div className="w-full bg-neutral-950 text-white">
       {/* Navbar */}
       <Navbar />
 
       {/* Outlet */}
-      <div className="p-2 w-full bg-neutral-950 dark:bg-white">
+      <div className="p-2 w-full">
         <Outlet />
       </div>
     </div>
