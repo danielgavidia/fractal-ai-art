2bb85f041e89379d733f882f79c941867dee6c55
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-24 17:53:29 -0400

    Added ArtCard component

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,16 +1,11 @@
 import { useEffect, useState } from "react";
-import BouncingBall from "./BouncingBall";
 import { getArtworks, postLike } from "../utils/expressUtils";
 import { Artwork } from "../types/types";
-import { useNavigate } from "react-router-dom";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader } from "./ui/card";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import ArtCard from "./ArtCard";
 
 const Feed = () => {
   const [artworks, setArtworks] = useState<Artwork[]>([]);
   const [liked, setLiked] = useState<boolean>(false);
-  const navigate = useNavigate();
 
   // Reload configs
   useEffect(() => {
@@ -34,74 +29,9 @@ const Feed = () => {
     <div className="bg-stone-200 overflow-y-scroll space-y-4 flex flex-col justify-center items-center h-full py-6">
       {artworks
         .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-        .map((artwork, key) => {
-          const {
-            id,
-            user,
-            createdAt,
-            likesCount,
-
-            // Artwork config
-            xVelocity,
-            yVelocity,
-            ballSize,
-            ballColor,
-            backgroundColor,
-            ballCount,
-            randomnessFactor,
-            randomColors,
-            borderRadius,
-            borderWidth,
-            borderColor,
-          } = artwork;
-          return (
-            <Card
-              key={key}
-              className="bg-neutral-400 flex flex-col border-[0.5px] border-neutral-400 shadow-lg bg-stone-50"
-            >
-              <CardHeader>
-                <CardDescription className="flex items-center space-x-2 text-neutral-400">
-                  <button onClick={() => navigate(`/profile/${user?.id}`)} className="text-sm">
-                    {user?.email}
-                  </button>
-                  <p className="test-sm">·</p>
-                  <p className="text-sm">
-                    {new Date(createdAt).toLocaleDateString("en-US", {
-                      month: "short",
-                      day: "numeric",
-                      year: "numeric",
-                    })}
-                  </p>
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="border-[0.5px] border-black">
-                  <BouncingBall
-                    xVelocity={xVelocity}
-                    yVelocity={yVelocity}
-                    ballSize={ballSize}
-                    ballColor={ballColor}
-                    backgroundColor={backgroundColor}
-                    ballCount={ballCount}
-                    randomnessFactor={randomnessFactor}
-                    randomColors={randomColors}
-                    borderRadius={borderRadius}
-                    borderWidth={borderWidth}
-                    borderColor={borderColor}
-                  />
-                </div>
-              </CardContent>
-
-              <CardFooter className="flex space-x-2 pt-2 justify-center">
-                <button onClick={() => handleLike(id)} className="text-neutral-400">
-                  <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
-                </button>
-
-                <p className="p-2 text-neutral-400">{likesCount}</p>
-              </CardFooter>
-            </Card>
-          );
-        })}
+        .map((artwork, key) => (
+          <ArtCard key={key} artwork={artwork} handleLike={handleLike} />
+        ))}
     </div>
   );
 };
