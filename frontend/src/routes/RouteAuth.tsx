b74dae83080b8d49175e05b2d0e22039aa5ba5ff
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-24 22:38:39 -0400

    Fixed login/signup pages

--- frontend/src/routes/RouteAuth.tsx
+++ frontend/src/routes/RouteAuth.tsx
@@ -2,16 +2,16 @@ import { useParams } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 const RouteAuth = () => {
-	const { authOperation } = useParams();
-	return (
-		<div>
-			{authOperation === "login" || authOperation === "signup" ? (
-				<AuthForm authOperation={authOperation} />
-			) : (
-				<></>
-			)}
-		</div>
-	);
+  const { authOperation } = useParams();
+  return (
+    <div className="h-full">
+      {authOperation === "login" || authOperation === "signup" ? (
+        <AuthForm authOperation={authOperation} />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default RouteAuth;
